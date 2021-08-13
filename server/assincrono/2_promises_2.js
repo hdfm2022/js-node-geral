faf98dd1f0e6fb174196f3c48d92207c8a44c301
hdfm2022 <hdfmacedo+git@gmail.com>
2021-08-13 13:45:19 -0300

    praticando + promises

--- server/assincrono/2_promises_2.js
+++ server/assincrono/2_promises_2.js
@@ -1,4 +1,23 @@
 // pra executar:
 // node .\server\assincrono\2_promises_2.js
 
- // https://www.youtube.com/watch?v=DHvZLI7Db8E
+// https://www.youtube.com/watch?v=DHvZLI7Db8E
+
+let p = new Promise((resolve, reject) => {
+    let a = 1 + 1
+    if (a == 2) {
+        resolve("Success");
+    } else {
+        reject("Failed");
+    }
+})
+
+console.log("oi 1");
+
+p.then((message) => {
+    console.log("Success: " + message)
+}).catch((message) => {
+    console.log("Failed: " + message)
+})
+
+console.log("oi 2");
